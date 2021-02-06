5eea1c4fa29aa9308aeb7485e15933b2f8a7256a
Aaryan Gattani <69848370+AaryanGattani@users.noreply.github.com>
2021-02-06 10:15:42 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,42 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+    this.reset = createButton('Reset');
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+  }
+
+  display(){
+    imageMode(CENTER);
+    image(startBG,displayWidth/2,displayHeight/2,displayWidth,displayHeight);
+    
+    this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
+    this.button.position(displayWidth/2 + 15, displayHeight/2);
+    this.reset.position(displayWidth-100,20);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2 - 40, displayHeight/2 -80);
+    });
+
+    this.reset.mousePressed(()=>{
+      player.updateCount(0);
+      game.update(0);
+    });
+
+  }
+}
